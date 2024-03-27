dd762396bc73b6fec8accf8e7631383e4134ef6a
SachinRawatLearning <sachinrawatdmc@gmail.com>
2024-03-27 11:44:01 +0530

    Added Fraction Functionality

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { numberToFraction } from '../helper.js';
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -133,7 +133,7 @@ class RecipeView extends View {
         </svg>
         <div class="recipe__quantity">${
           ingredient.quantity
-            ? new Fraction(ingredient.quantity).toString()
+            ? numberToFraction(ingredient.quantity).toString()
             : ''
         }</div>
         <div class="recipe__description">
